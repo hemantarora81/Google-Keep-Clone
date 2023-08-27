9f7290f43462c6533fad854f65b65cae913995bf
Hemant Arora <arorahemant75@gmail.com>
2023-08-27 15:52:34 +0530

    responsive

--- src/Components/Notes/Notes.jsx
+++ src/Components/Notes/Notes.jsx
@@ -1,61 +1,67 @@
-import { Box, Grid } from '@mui/material'
-import { DragDropContext,Droppable,Draggable } from 'react-beautiful-dnd';
-import { styled } from '@mui/material/styles';
-import React,{useContext} from 'react'
-import {Form,Note,EmptyNotes} from '../'
-import { DataContext } from '../../Context/DataProvider';
-import {reorder} from '../../utils/commonutils'
+import { Box, Grid } from "@mui/material";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { styled } from "@mui/material/styles";
+import React, { useContext } from "react";
+import { Form, Note, EmptyNotes } from "../";
+import { DataContext } from "../../Context/DataProvider";
+import { reorder } from "../../utils/commonutils";
 const Notes = () => {
-    const {notes,setNotes} = useContext(DataContext)
-    const DrawerHeader = styled('div')(({ theme }) => ({
-  
-        // necessary for content to be below app bar
-        ...theme.mixins.toolbar,
-    }));
-  
-    const onDragEnd = (result) => {
-        if (!result.destination) 
-          return;
-    
-        const items = reorder(notes, result.source.index, result.destination.index);    
-        setNotes(items);
-    }
+  const { notes, setNotes } = useContext(DataContext);
+  const DrawerHeader = styled("div")(({ theme }) => ({
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  }));
+
+  const onDragEnd = (result) => {
+    if (!result.destination) return;
+
+    const items = reorder(notes, result.source.index, result.destination.index);
+    setNotes(items);
+  };
   return (
-    <Box sx={{ display: 'flex', width: '100%' }}>
-    <Box sx={{ p: 3, width: '100%' }}>
+    <Box sx={{ display: "flex", width: "100%" }}>
+      <Box sx={{ p: 3, width: "100%" }}>
         <DrawerHeader />
         <Form />
-        { notes.length > 0 ? 
-            <DragDropContext onDragEnd={onDragEnd}>
-                <Droppable droppableId="droppable">
-                    {(provided, snapshot) => (
-                        <Grid container style={{ marginTop: 16}}
-                            {...provided.droppableProps}
-                            ref={provided.innerRef}
+        {notes.length > 0 ? (
+          <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable droppableId="droppable">
+              {(provided, snapshot) => (
+                <Grid
+                  id="hemanttttttt"
+                  container
+                  style={{ marginTop: 16 }}
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                >
+                  {notes.map((note, index) => (
+                    <Draggable
+                      key={note.id}
+                      draggableId={note.id}
+                      index={index}
+                    >
+                      {(provided, snapshot) => (
+                        <Grid
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          item
                         >
-                        {
-                            notes.map((note, index) => (
-                                <Draggable key={note.id} draggableId={note.id} index={index}>
-                                    {(provided, snapshot) => (
-                                        <Grid ref={provided.innerRef}
-                                            {...provided.draggableProps}
-                                            {...provided.dragHandleProps}
-                                            item
-                                        >
-                                            <Note note={note} />
-                                        </Grid>
-                                    )}
-                                </Draggable >
-                            ))
-                        }
+                          <Note note={note} />
                         </Grid>
-                    )}
-                </Droppable >
-            </DragDropContext>
-        : <EmptyNotes /> }
+                      )}
+                    </Draggable>
+                  ))}
+                </Grid>
+              )}
+            </Droppable>
+          </DragDropContext>
+        ) : (
+          <EmptyNotes />
+        )}
+      </Box>
     </Box>
-</Box>
-  )
-}
+  );
+};
 
-export default Notes
+export default Notes;
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,75 +1,67 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MuiDrawer from '@mui/material/Drawer';
-import {Header, NewList} from './'
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MuiDrawer from "@mui/material/Drawer";
+import { Header, NewList } from "./";
 const drawerWidth = 240;
 
 const openedMixin = (theme) => ({
   width: drawerWidth,
-  transition: theme.transitions.create('width', {
+  transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.enteringScreen,
   }),
-  overflowX: 'hidden',
+  overflowX: "hidden",
 });
 
 const closedMixin = (theme) => ({
-  transition: theme.transitions.create('width', {
+  transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
-  overflowX: 'hidden',
+  overflowX: "hidden",
   width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up('sm')]: {
+  [theme.breakpoints.up("sm")]: {
     width: `calc(${theme.spacing(8)} + 1px)`,
   },
 });
 
-const DrawerHeader = styled('div')(({ theme }) => ({
+const DrawerHeader = styled("div")(({ theme }) => ({
   ...theme.mixins.toolbar,
 }));
 
-
-
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-    boxSizing: 'border-box',
-    ...(open && {
-      ...openedMixin(theme),
-      '& .MuiDrawer-paper': openedMixin(theme),
-    }),
-    ...(!open && {
-      ...closedMixin(theme),
-      '& .MuiDrawer-paper': closedMixin(theme),
-    }),
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
   }),
-);
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
+}));
 
- const  Sidebar=() =>{
-  const [open, setOpen] = React.useState(true);
+const Sidebar = () => {
+  const [open, setOpen] = React.useState(false);
 
   const handleDrawer = () => {
-    setOpen(prevState=> !prevState);
+    setOpen((prevState) => !prevState);
   };
 
- 
-
   return (
-    <Box sx={{ display: 'flex' }}>
-          <Header open={open} handleDrawer={handleDrawer} />
+    <Box sx={{ display: "flex" }}>
+      <Header open={open} handleDrawer={handleDrawer} />
       <Drawer variant="permanent" open={open}>
-        <DrawerHeader>
-         
-        </DrawerHeader>
-              <NewList open={open} />
-       
+        <DrawerHeader></DrawerHeader>
+        <NewList open={open} />
       </Drawer>
- 
     </Box>
   );
-}
+};
 export default Sidebar;
